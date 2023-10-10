c78ea33a662e358f1d4bb6b31faeb4e90c77d06c
HenriqueFM96 <henriquefm96@hotmail.com>
2023-10-10 15:51:53 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -44,3 +44,49 @@ resource "aws_subnet" "hamgmtsubnet" {
     Name = "hamgmt subnet"
   }
 }
+
+#############################################################################################################
+#                                          SPOKE 01 - VPC                                                   #
+#############################################################################################################
+resource "aws_vpc" "spoke_vpc1" {
+  cidr_block = var.spoke_vpc1_cidr
+
+  tags = {
+    Name     = "${var.tag_name_prefix}-vpc-spoke01"
+    scenario = var.scenario
+  }
+}
+
+# Subnets
+resource "aws_subnet" "spoke_vpc1-priv1" {
+  vpc_id            = aws_vpc.spoke_vpc1.id
+  cidr_block        = var.spoke_vpc1_private_subnet_cidr1
+  availability_zone = var.availability_zone1
+
+  tags = {
+    Name = "${aws_vpc.spoke_vpc1.tags.Name}-priv1"
+  }
+}
+
+# Routes
+resource "aws_route_table" "spoke1-rt" {
+  vpc_id = aws_vpc.spoke_vpc1.id
+
+  route {
+    cidr_block         = "0.0.0.0/0"
+    transit_gateway_id = aws_ec2_transit_gateway.TGW-XAZ.id
+  }
+
+  tags = {
+    Name     = "spoke-vpc1-rt"
+    scenario = var.scenario
+  }
+  depends_on = [aws_ec2_transit_gateway.TGW-XAZ]
+}
+
+# Route tables associations
+resource "aws_route_table_association" "spoke1_rt_association1" {
+  subnet_id      = aws_subnet.spoke_vpc1-priv1.id
+  route_table_id = aws_route_table.spoke1-rt.id
+}
+
